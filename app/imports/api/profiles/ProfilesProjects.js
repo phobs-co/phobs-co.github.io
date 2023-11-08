bac2ef173a658253233e6ab6748a0dafd672a992
Heinrich Maertens <no-reply@mail.heinrich.ninja>
2023-11-07 18:33:04 -1000

    Initialize dependabot/npm_and_yarn/app/path-parse-1.0.7

---
+++ app/imports/api/profiles/ProfilesProjects.js
@@ -0,0 +1,25 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Encapsulates state and variable values for this collection. */
+class ProfilesProjectsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ProfilesProjectsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      profile: String,
+      project: String,
+    }, { tracker: Tracker });
+    // Ensure collection documents obey schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const ProfilesProjects = new ProfilesProjectsCollection();
